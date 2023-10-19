22d395a5110b3ea8e62f4b0593dced86f0f07ab6
Gabriel Barrientos <gabri12.rubik@gmail.com>
2023-10-19 19:10:06 -0300

    Agregar atajos para gitsigns

--- nvim/lua/plugins/git.lua
+++ nvim/lua/plugins/git.lua
@@ -1,24 +1,12 @@
 return {
   {
     'lewis6991/gitsigns.nvim',
-    config = true
-  },
-  {
-    'github/copilot.vim',
     config = function()
-      vim.g.copilot_assume_mapped = true
-      vim.g.copilot_enabled = 0
-
-      -- Atajo para alternar el estado de Copilot
-      vim.keymap.set('n', '<leader>tc', function()
-        if vim.g.copilot_enabled == 1 then
-          vim.cmd('Copilot disable')
-          print('Copilot disabled')
-        else
-          vim.cmd('Copilot enable')
-          print('Copilot enabled')
-        end
-      end, { desc = 'Toggle Copilot' })
-    end,
+      local gs = require('gitsigns')
+      gs.setup()
+      vim.keymap.set('n', '(h', gs.prev_hunk)
+      vim.keymap.set('n', ')h', gs.next_hunk)
+      vim.keymap.set('n', '<leader>gd', gs.diffthis)
+    end
   }
 }
