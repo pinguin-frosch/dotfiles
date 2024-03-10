6b1511cd37a1d50dce1f736cf07c85c4d6118006
Gabriel Barrientos <gabri12.rubik@gmail.com>
2024-03-10 17:57:53 -0300

    Cambiar opciones de neovim

--- nvim/lua/config/autocommands.lua
+++ nvim/lua/config/autocommands.lua
@@ -7,24 +7,3 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   group = highlight_group,
   pattern = '*',
 })
-
--- Cerrar páginas de ayuda con q
-local close_files = vim.api.nvim_create_augroup('CloseFiles', { clear = true })
-vim.api.nvim_create_autocmd('FileType', {
-  callback = function()
-    vim.keymap.set('n', 'q', '<cmd>q<cr>', { buffer = true, silent = true })
-  end,
-  group = close_files,
-  pattern = 'help',
-})
-
--- Quitar números en terminal
-local no_numbers = vim.api.nvim_create_augroup('NoNumbers', { clear = true })
-vim.api.nvim_create_autocmd('TermOpen', {
-  callback = function()
-    vim.opt.number = false
-    vim.opt.relativenumber = false
-  end,
-  group = no_numbers,
-  pattern = '*',
-})
