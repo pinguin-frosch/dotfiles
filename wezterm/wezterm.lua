6a1c2f55a45f887aad7d0881236b02e3e72cfd8a
Gabriel Barrientos <gabri12.rubik@gmail.com>
2024-03-10 18:00:52 -0300

    Desactivar ligaduras en la terminal

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -10,31 +10,12 @@ local config = {
     color_scheme = theme,
     force_reverse_video_cursor = true,
     audible_bell = 'Disabled',
+    harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' },
     window_padding = {
         left = 0,
         right = 0,
         top = 0,
         bottom = 0
-    },
-}
-
--- Alternar ligaduras
-wezterm.on('toggle-ligature', function(window, _)
-    local overrides = window:get_config_overrides() or {}
-    if not overrides.harfbuzz_features then
-        overrides.harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' }
-    else
-        overrides.harfbuzz_features = nil
-    end
-    window:set_config_overrides(overrides)
-end)
-
--- Atajos de teclado
-config.keys = {
-    {
-        key = 'L',
-        mods = 'ALT',
-        action = wezterm.action.EmitEvent('toggle-ligature'),
     },
 }
 
