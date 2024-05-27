dc684414156a3b438c999aac494c423d54627bcd
Gabriel Barrientos <gabri12.rubik@gmail.com>
2024-05-26 21:39:15 -0400

    Agregar pyenv para trabajar con python

--- plasma-workspace/env/env.sh
+++ plasma-workspace/env/env.sh
@@ -15,8 +15,9 @@ export NPM_CONFIG_USERCONFIG=$XDG_CONFIG_HOME/npm/npmrc
 export RUSTUP_HOME=$XDG_DATA_HOME/rustup
 export R_ENVIRON_USER=$XDG_CONFIG_HOME/R/Renviron
 export _JAVA_OPTIONS=-Djava.util.prefs.userRoot=$XDG_CONFIG_HOME/java
+export PYENV_ROOT=$XDG_DATA_HOME/pyenv
 
 # Variables de entorno
 export EDITOR=nvim
-export PATH=$PATH:$HOME/.local/bin:$GOPATH/bin:$CARGO_HOME/bin
+export PATH=$PYENV_ROOT/bin:$HOME/.local/bin:$GOPATH/bin:$CARGO_HOME/bin:$PATH
 export SSH_AUTH_SOCK=$XDG_RUNTIME_DIR/ssh-agent.socket
