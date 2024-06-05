78ab1faaca2a856d22d9ae838db330b6193a0e22
Gabriel Barrientos <gabri12.rubik@gmail.com>
2024-06-05 11:45:14 -0400

    Formatear archivos python al usar pyright

---
+++ nvim/lua/lsp/pyright.lua
@@ -0,0 +1,13 @@
+local M = {}
+
+M.opts = {
+  on_attach = function(_, bufnr)
+    require('lsp.lsp').setup_keymaps(bufnr)
+    vim.keymap.del('n', '<leader>f', { buffer = bufnr })
+    vim.keymap.set('n', '<leader>f', function()
+      vim.cmd('silent !ruff format %')
+    end, { buffer = bufnr })
+  end
+}
+
+return M
