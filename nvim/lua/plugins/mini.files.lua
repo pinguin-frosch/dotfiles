cb317f96f4ddae8b82449d5dce859cad1e8ff862
Gabriel Barrientos <gabri12.rubik@gmail.com>
2025-07-31 12:56:15 -0400

    nvim: ampliar ventana en el explorador

--- nvim/lua/plugins/mini.files.lua
+++ nvim/lua/plugins/mini.files.lua
@@ -3,7 +3,11 @@ return {
     version = "*",
     config = function()
         local files = require('mini.files')
-        files.setup()
+        files.setup({
+            windows = {
+                width_focus = 80
+            }
+        })
         vim.keymap.set('n', '<leader>e', function()
             files.open()
         end, { desc = 'Open File Explorer' })
