8c07c1ac0cb4cde6a6a96ce69cbba0a088304de0
Gabriel Barrientos <gabri12.rubik@gmail.com>
2025-11-21 12:52:04 -0300

    nvim: mover lazy a otro paquete

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,31 +1,7 @@
 local theme = require('config.options')
 require('config.keymaps')
 require('config.autocommands')
-
--- Instalar el gestor de paquetes
-local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system {
-    'git',
-    'clone',
-    '--filter=blob:none',
-    'https://github.com/folke/lazy.nvim.git',
-    '--branch=stable',
-    lazypath,
-  }
-end
-vim.opt.rtp:prepend(lazypath)
-
--- Cargar plugins y cambiar el tema de lazy
-require('lazy').setup('plugins', {
-  install = {
-    colorscheme = { theme }
-  },
-  change_detection = {
-    notify = false
-  }
-})
-
+require('core.lazy')
 require('core.lsp')
 
 -- Cambiar el tema, tiene que ser después de cargar los plugins
---
+++ nvim/lua/core/lazy.lua
@@ -0,0 +1,20 @@
+-- Instalar el gestor de paquetes
+local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system {
+    'git',
+    'clone',
+    '--filter=blob:none',
+    'https://github.com/folke/lazy.nvim.git',
+    '--branch=stable',
+    lazypath,
+  }
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Cargar plugins y cambiar el tema de lazy
+require('lazy').setup('plugins', {
+  change_detection = {
+    notify = false
+  }
+})
