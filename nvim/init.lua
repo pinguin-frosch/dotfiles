787dfe133fea0654bdefbd3df21baa67fc49dc20
Gabriel Barrientos <gabri12.rubik@gmail.com>
2025-11-21 17:24:57 -0300

    nvim: move core to config

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,8 +1,8 @@
 local theme = require('config.options')
 require('config.keymaps')
 require('config.autocommands')
-require('core.lazy')
-require('core.lsp')
+require('config.lazy')
+require('config.lsp')
 
 -- Cambiar el tema, tiene que ser después de cargar los plugins
 vim.cmd('colorscheme ' .. theme)
