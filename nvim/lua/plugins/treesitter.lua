5284f132594024f4786cd5972126668faeea78e3
Gabriel Barrientos <gabri12.rubik@gmail.com>
2026-03-31 22:23:44 -0300

    nvim: usar nuevo sistema de highlighting y indenting de treesitter

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -3,10 +3,28 @@ return {
   version = false,
   build = ':TSUpdate',
   event = { 'BufRead', 'BufNewFile' },
+
+  -- inspirado en la configuracion de kartikvashistha
+  -- https://github.com/kartikvashistha/ansible-playbooks/blob/6b3d5df880782ddd40a6349e2113272b67ce8c55/roles/neovim/files/nvim/lua/plugins/editor.lua#L51-L80
   config = function()
-    require('nvim-treesitter.configs').setup({
-      highlight = { enable = true },
-      indent = { enable = true },
+    vim.api.nvim_create_autocmd('FileType', {
+      group = vim.api.nvim_create_augroup('treesitter.setup', {}),
+      callback = function(args)
+        local buf = args.buf
+        local filetype = args.match
+        local language = vim.treesitter.language.get_lang(filetype) or filetype
+
+        -- ignorar lenguages sin soporte de treesitter
+        if not vim.treesitter.language.add(language) then
+          return
+        end
+
+        -- activar highlighting
+        vim.treesitter.start(buf, language)
+
+        -- activar indentado
+        vim.bo[buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+      end
     })
   end,
 }
