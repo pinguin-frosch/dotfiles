13004536e9aeb107eedc6875da5df8c05f79799e
Gabriel Barrientos <gabri12.rubik@gmail.com>
2026-04-17 13:26:39 -0400

    nvim(lsp): move custom format logic to the same LspAttach to remove race conditions

--- nvim/lsp/gopls.lua
+++ nvim/lsp/gopls.lua
@@ -1,26 +1,3 @@
-vim.api.nvim_create_autocmd('LspAttach', {
-  pattern = "*.go",
-  group = vim.api.nvim_create_augroup('goimports', {}),
-  callback = function(args)
-    local client = vim.lsp.get_client_by_id(args.data.client_id)
-    local enc = client and client.offset_encoding or 'utf-16'
-    vim.keymap.set('n', '<leader>lf', function()
-      local params = vim.lsp.util.make_range_params(0, enc)
-      ---@diagnostic disable-next-line: inject-field
-      params.context = { only = { "source.organizeImports" } }
-      local result = vim.lsp.buf_request_sync(args.buf, "textDocument/codeAction", params)
-      for _, res in pairs(result or {}) do
-        for _, r in pairs(res.result or {}) do
-          if r.edit then
-            vim.lsp.util.apply_workspace_edit(r.edit, enc)
-          end
-        end
-      end
-      vim.lsp.buf.format({ async = false })
-    end, { buffer = args.buf })
-  end
-})
-
 return {
   settings = {
     gopls = {
--- nvim/lua/config/lsp.lua
+++ nvim/lua/config/lsp.lua
@@ -10,6 +10,44 @@ vim.keymap.del('n', 'grr')
 vim.keymap.del('n', 'grt')
 vim.keymap.del('n', 'gO')
 
+---@alias FormatFnFactory fun(args: vim.api.keyset.create_autocmd.callback_args): function
+---@type table<string, FormatFnFactory>
+local language_format_functions = {
+  go = function(args)
+    local client = vim.lsp.get_client_by_id(args.data.client_id)
+    local enc = client and client.offset_encoding or 'utf-16'
+    return function()
+      local params = vim.lsp.util.make_range_params(0, enc)
+      ---@diagnostic disable-next-line: inject-field
+      params.context = { only = { "source.organizeImports" } }
+      local result = vim.lsp.buf_request_sync(args.buf, "textDocument/codeAction", params)
+      for _, res in pairs(result or {}) do
+        for _, r in pairs(res.result or {}) do
+          if r.edit then
+            vim.lsp.util.apply_workspace_edit(r.edit, enc)
+          end
+        end
+      end
+      vim.lsp.buf.format({ async = false })
+    end
+  end,
+}
+
+---@param args vim.api.keyset.create_autocmd.callback_args
+---@return function
+local function get_language_format_function(args)
+  local filetype = vim.bo[args.buf].filetype
+  local factory_fn = language_format_functions[filetype]
+
+  -- execute to generate a ready to use function to format
+  if factory_fn then
+    return factory_fn(args)
+  end
+
+  -- base case if there is no custom format function
+  return vim.lsp.buf.format
+end
+
 vim.api.nvim_create_autocmd('LspAttach', {
   group = vim.api.nvim_create_augroup('LspKeymaps', { clear = true }),
   callback = function(ev)
@@ -29,8 +67,7 @@ vim.api.nvim_create_autocmd('LspAttach', {
     nmap('gD', vim.lsp.buf.type_definition, 'Goto Type Definition')
     nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
     nmap('<C-k>', vim.lsp.buf.signature_help, 'Signature Documentation')
-    nmap('<leader>lf', function()
-      vim.lsp.buf.format({ async = true })
-    end, 'Format File')
+    local format_function = get_language_format_function(ev)
+    nmap('<leader>lf', format_function, 'Format File')
   end
 })
